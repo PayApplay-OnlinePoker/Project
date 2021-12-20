3f97a7af4ec4c15faf7fc532a1dac8e7465f0306
qstion05 <qstion05@gmail.com>
2021-12-20 22:25:16 +0900

    2021.12.20 make basement func in server code

---
+++ server_func.py
@@ -0,0 +1,49 @@
+def join(userID, roomID, roomPW):
+    pass
+
+def create(userID, roomName, roomPW, baseBetting, baseMoney):
+    pass
+
+def leave(userID):
+    pass
+
+class bet:
+    def check(self):
+        pass
+
+    def call(self):
+        pass
+
+    def quarter(self, money):
+        pass
+
+    def half(self, money):
+        pass
+
+    def allin(self, money):
+        pass
+
+    def fold(self):
+        pass
+
+
+def drawCard(card, isHidden):
+    pass
+
+def removeCard(card):
+    pass
+
+def openCard(card):
+    pass
+
+def startGame():
+    pass
+
+def checkUserMoney(userID, money):
+    pass
+
+def checkTableMoney(money):
+    pass
+
+def winner(userID, nickname):
+    pass
