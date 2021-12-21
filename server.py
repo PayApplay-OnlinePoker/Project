63b0bd0558ebdbd81a60915b3edf1dccad3fc8c2
LJLee37 <ljlee3759@gmail.com>
2021-12-22 00:15:52 +0900

    Add threading parts.
    Now a new TCP connection makes a new thread

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import socket
 COMMANDS = ['join', 'create', 'leave', 'bet', 'drawCard', 'removeCard', 'openCard', 'startGame', 'checkUserMoney', 'checkTableMoney', 'winner']
 PORT = 31597
 MAX_USERS = 100
+playerHandler = PlayerHandler()
 
 
 class Room:
@@ -22,8 +23,21 @@ class Player:
         self.nickname = nickname
         self.socket = socket
 
+class PlayerHandler:
+    def __init__(self):
+        self.players = []
+        self.playerCount = 0
 
-def accept_tcp_connection():
+
+def listen_tcp_connection():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
         serverSocket.bind(('localhost', PORT))
-        serverSocket.listen()
+        serverSocket.listen(MAX_USERS)
+        while True:
+            newConnection, newConnectionAddr = serverSocket.accept()
+            newThread = threading.Thread(target=accept_tcp_connection, args=(newConnection, newConnectionAddr))
+            newThread.start()
+
+def accept_tcp_connection(newConnection, newConnectionAddr):
+    while True:
+        clientMessage = newConnection.recv(2048)
