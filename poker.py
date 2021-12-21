582d1d5d971b95b17725f49bb78ef2fd5840be61
qstion05 <qstion05@gmail.com>
2021-12-21 23:13:09 +0900

    12.21 23:13 fix

--- poker.py
+++ poker.py
@@ -8,15 +8,13 @@ import random
 HAND_RANKING = ["HC", "OP", "TP", "TK", "S", "BS", "MT", "F", "FH", "FC", "SF", "BSF", "RSF"] 
 #High Card(Top)->One pair->Two pair->Three of kind(Triple)->Straight->Back Straight->Mountain->Flush->Full House->Four cards->Straight Flush->Back Straight Flush->Royal Straight Flush
 
-class CheckVal(self):
+class CheckVal:
     def check_winner(ID, nickname):
         pass 
 
 class Gameplay:
     def __init__(self):
         self.generate_deck()
-
-
 
     def generate_deck(self): #generate card deck
         deck = [i for i in range(1, 53)] 
