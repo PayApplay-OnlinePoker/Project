4fa394cc90799baddffc6792dcc2f62a374178a3
qstion05 <qstion05@gmail.com>
2021-12-21 12:47:13 +0900

    card shuffle/draw mechanism set

--- poker.py
+++ poker.py
@@ -7,15 +7,22 @@ import random
 card = [i for i in range(1, 53)] #카드 덱 생성
 hand_Rankings = [] #카드 족보
 
-# 카드 뽑기
-# 1. 리스트를 셔플 한 뒤, 처음 리스트부터 뽑는 방법.
-shuffledCard = random.shuffle(card)
-for i in range(53):
-	drawCard = shuffled_card[i]
+print(card)
 
-# 2. 리스트중 랜덤으로 하나를 뽑는 방법.
-for i in range(53):
-drawCard = random.choice(card)
+# 카드 뽑기
+# 1. 리스트중 랜덤으로 하나를 뽑는 방법.
+print("-------------1. 리스트중 랜덤으로 하나를 뽑는 방법. ---------------")
+for i in range(52):
+    drawCard = random.choice(card)
+    print(drawCard)
+
+# 2. 리스트를 셔플 한 뒤, 처음 리스트부터 뽑는 방법.
+print("-------------2. 리스트를 셔플 한 뒤, 처음 리스트부터 뽑는 방법.---------------")
+random.shuffle(card)
+for i in range(52):
+    drawCard = card[i]
+    print(drawCard)
+
 
 
 
