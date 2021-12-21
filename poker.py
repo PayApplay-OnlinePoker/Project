1466074aba641bad34e7053e89f2da976d921fad
qstion05 <qstion05@gmail.com>
2021-12-21 23:20:37 +0900

    12/21 23:20 fixed

--- poker.py
+++ poker.py
@@ -5,29 +5,29 @@
 #S-H-D-C
 
 import random
-HAND_RANKING = ["HC", "OP", "TP", "TK", "S", "BS", "MT", "F", "FH", "FC", "SF", "BSF", "RSF"] 
+HAND_RANKING = ["HC", "OP", "TP", "TK", "S", "BS", "MT", "F", "FH", "FC", "SF", "BSF", "RSF"]
 #High Card(Top)->One pair->Two pair->Three of kind(Triple)->Straight->Back Straight->Mountain->Flush->Full House->Four cards->Straight Flush->Back Straight Flush->Royal Straight Flush
 
 class CheckVal:
-    def check_winner(ID, nickname):
-        pass 
+    def check_winner(self,ID, nickname):
+        pass
 
-class Gameplay:
+class Gameplay(CheckVal):
     def __init__(self):
         self.generate_deck()
 
     def generate_deck(self): #generate card deck
-        deck = [i for i in range(1, 53)] 
+        deck = [i for i in range(1, 53)]
         self.deck = deck
 
 
     def card_draw(self): #draw a card
-        # 1. draw a card randomly from a list 
+        # 1. draw a card randomly from a list
         drawCard = random.choice(self.deck)
         self.deck.remove(drawCard)
         return drawCard
     
-    def calculate_ranking(self): #hand-ranking 
+    def calculate_ranking(self): #hand-ranking
         global HAND_RANKING
         pass
 
