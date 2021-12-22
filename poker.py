1a7f8e6cb33a8ea33c822b642a6351673ff2d2e7
qstion05 <qstion05@gmail.com>
2021-12-22 14:29:54 +0900

    make poker process

--- poker.py
+++ poker.py
@@ -6,6 +6,8 @@
 
 import random
 HAND_RANKING = ["HC", "OP", "TP", "TK", "S", "BS", "MT", "F", "FH", "FC", "SF", "BSF", "RSF"]
+CLIENT_CARD = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+CLIENT_PATTEN = ["Spade", "Heart", "Diamond", "Clover"]
 #High Card(Top)->One pair->Two pair->Three of kind(Triple)->Straight->Back Straight->Mountain->Flush->Full House->Four cards->Straight Flush->Back Straight Flush->Royal Straight Flush
 
 class Apicall:
@@ -61,3 +63,61 @@ class Gameplay:
     def calculate_ranking(self): #hand-ranking
         global HAND_RANKING
         pass
+
+
+def number_to_card(number):
+    if number in range(1, 14): #Spade
+        this_card = CLIENT_PATTEN[0] + CLIENT_CARD[number - 1]
+        return this_card
+    elif number in range(14, 27): #Heart
+        this_card = CLIENT_PATTEN[1] + CLIENT_CARD[number - 14]
+        return this_card
+    elif number in range(27, 40): #Diamond
+        this_card = CLIENT_PATTEN[2] + CLIENT_CARD[number - 27]
+        return this_card
+    elif number in range(40, 53): #Clover
+        this_card = CLIENT_PATTEN[3] + CLIENT_CARD[number - 40]
+        return this_card
+
+def print_player_hand():
+    global playerHand
+    print("지금 가지고 계신 패는",  ','.join(playerHand) ,'입니다.')
+
+
+#4장을 준다
+test = Gameplay()
+playerHand = []
+
+#초기 패 설정
+for count in range(4): 
+    playerHand.append(number_to_card(test.card_draw()))
+print_player_hand()
+
+#카드 한장 버리기
+print("1:" + playerHand[0], "2:" + playerHand[1], "3:" + playerHand[2], "4:" + playerHand[3])
+removeCard = int(input("버릴 카드를 선택해주세요. : "))
+del playerHand[removeCard - 1]
+
+#카드 한장 오픈하기
+print("1:" + playerHand[0], "2:" + playerHand[1], "3:" + playerHand[2])
+
+openCard = int(input("오픈할 카드를 선택해주세요. : "))
+playerHand[openCard -1] = playerHand[openCard - 1] + "(open)"
+
+#SWAP
+swap = playerHand[0] 
+playerHand[0] = playerHand[openCard-1]
+playerHand[openCard -1 ] = swap
+print_player_hand()
+
+#오픈 카드 3장 주기.
+for playTurn in range(4, 7):
+    print(playTurn, "번째 카드입니다.")
+    playerHand.append(number_to_card(test.card_draw()))
+    playerHand[-1] = playerHand[-1] + "(open)"
+print_player_hand()
+
+#히든 카드 1장 주기.
+print("7번째 카드 입니다.")
+playerHand.append(number_to_card(test.card_draw()))
+print_player_hand()
