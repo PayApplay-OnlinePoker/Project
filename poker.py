372df583ad3557d6afd664cac45988816917522f
qstion05 <qstion05@gmail.com>
2021-12-23 16:47:45 +0900

    join, create CUI setting

--- poker.py
+++ poker.py
@@ -71,7 +71,7 @@ def number_to_card(number):
     elif number in range(14, 27): #Heart
         this_card = CLIENT_PATTEN[1] + CLIENT_CARD[number - 14]
         return this_card
-    elif number in range(27, 40): #Diamond
+    elif number in range(27, 40): #Diamond 
         this_card = CLIENT_PATTEN[2] + CLIENT_CARD[number - 27]
         return this_card
     elif number in range(40, 53): #Clover
@@ -86,6 +86,66 @@ def print_player_hand():
 #4장을 준다
 test = Gameplay()
 playerHand = []
+
+#CUI Part
+'''
+일단 서버와의 통신 없이.  CUI만 구현, 이후 통신이 구현 되면 필요한 부분 수정.
+'''
+userList = []
+roomList = {"Hello" : 1234}
+nickname = input("닉네임을 입력하세요. : ")
+while nickname in userList: #리스트에 있는 유저 닉네임과 비교, 만약 같은 닉네임이 있으면 생성 제한.
+    print("이미 존재하는 닉네임입니다.")
+    nickname = input("닉네임을 다시 입력하세요. : ")
+
+while True:
+    print("방 생성과 방 참가 중 하나를 선택해주세요. ")
+    joinOrCreate = int(input("1.방 생성, 2.방 참가, 3.종료: "))
+
+    if joinOrCreate == 1:#create
+
+        print("방을 생성합니다. ")
+        createRoomName = input("방 제목을 입력해주세요: ")
+
+        createRoomPW = input("방 비밀번호를 입력해주세요: ")
+
+        BASEMONEY = [50, 75, 100]
+        print("초기 소지 금액을 선택해주세요.")
+        choiceBaseMoney = int(input("1. 50$, 2. 75$, 3. 100$ : "))
+        while choiceBaseMoney not in range(1, 4):
+            print("1번, 2번, 3번 중 하나를 선택해주세요.")
+        createBaseMoney = int(BASEMONEY[choiceBaseMoney -1])
+
+        print("기본 베팅 금액을 설정해주세요. ")
+        createBaseBetting = int(input("(1~10사이 숫자를 입력하세요.): "))
+        while createBaseBetting not in range(1, 11):
+            print("1~10사이 숫자를 입력해주세요.")
+        print("호스트:", nickname, ", 방 제목:", createRoomName , ", 비밀번호:",createRoomPW, ", 초기 소지 금액:", str(createBaseMoney) +"$",", 기본 베팅 금액:", str(createBaseBetting) +"$")
+        print("방이 생성되었습니다.")    
+        break
+
+
+
+    elif joinOrCreate == 2:#join
+        joinRoomName = input("방 제목을 입력해주세요: ")
+        if joinRoomName in roomList:
+            joinRoomPW = int(input("비밀번호를 입력해주세요: "))
+            if joinRoomPW == roomList[joinRoomName]:
+                print("룸에 입장하셨습니다.")
+                break
+            else:
+                print("비밀번호가 일치하지 않습니다.")
+                continue
+        else:
+            print("방이 존재하지 않습니다.")
+            continue
+
+    elif joinOrCreate == 3:
+        print("게임을 종료합니다.")
+        quit()
+
+    else:
+        print("잘못된 입력입니다.")
 
 
 #초기 패 설정
