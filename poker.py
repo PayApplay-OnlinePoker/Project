624963bce3bff56135c761380447dcb47045b626
qstion05 <qstion05@gmail.com>
2021-12-27 17:22:56 +0900

    helpmeplz

--- poker.py
+++ poker.py
@@ -25,7 +25,7 @@ beforeCredit = 0
 #High Card(Top)->One pair->Two pair->Three of kind(Triple)->Straight->Back Straight->Mountain->Flush->Full House->Four cards->Straight Flush->Back Straight Flush->Royal Straight Flush
 
 def print_player_hand(playerHand, Name):
-    print(Name," 님께서 지금 가지고 계신 패는",  ','.join(playerHand) ,'입니다.')
+    print(Name,"님께서 지금 가지고 계신 패는",  ','.join(playerHand) ,'입니다.')
 
 def print_open_card(openCardList, Name):
     print(Name,"님의 오픈된 카드는", ",".join(openCardList), "입니다.")
@@ -77,12 +77,13 @@ def makeHandCard(handCardList, playerHandNum, playerHandPattern):
 def compareRank(rankList):
     global RANK
     global CARD_PATTEN
-    global playerList
+    global betters
+
     highestRankIdx = 0
     if len(rankList) == 1:
         return rankList[0]
     else:
-        for i in range(len(playerList)):
+        for i in range(len(betters)):
             if RANK.index(rankList[highestRankIdx][1][0]) < RANK.index(rankList[i][1][0]):
                 highestRankIdx = i
             elif RANK.index(rankList[highestRankIdx][1][0]) == RANK.index(rankList[i][1][0]):
@@ -97,28 +98,27 @@ def compareRank(rankList):
 def computer_betting(user, First):
     if First == True: #선 일때
         if RANK.index(user.playerHandRanking[0]) < 2:
-            return 1
+            temp = random.choice([1,3,3,3,4,4])
+            return temp
         elif RANK.index(user.playerHandRanking[0]) >= 5 and RANK.index(user.playerHandRanking[0]) < 10:
             return random.randint(3, 5)
 
         elif RANK.index(user.playerHandRanking[0]) >= 10:
-            return random.randint(3, 6)
+            return random.randint(3, 5)
 
     else: #후 일때
         if RANK.index(user.playerHandRanking[0]) < 2:
-            temp = random.randint(0, 16)
-            if temp >= 15:
-                return 6
-            else:
-                return 2
+            temp = random.randint(2, 4)
+            return temp
+
 
         elif RANK.index(user.playerHandRanking[0]) >= 5 and RANK.index(user.playerHandRanking[0]) < 10:
             return random.randint(3, 5)
 
         elif RANK.index(user.playerHandRanking[0]) >= 10:
-            return random.randint(3, 6)
+            return random.randint(3, 5)
 
-def betting(selectMenu, user, tableMoney, call):
+def betting(selectMenu, user, tableMoney):
     global beforeBetting
     global beforeCredit
     global betters
@@ -126,64 +126,66 @@ def betting(selectMenu, user, tableMoney, call):
     global foldedList
     bettingMoney = 0
 
-    if user.money == 0:
+    if user.playerMoney == 0:
         print("소지금이 0원입니다.")
         return bettingMoney
 
     if selectMenu == 1: #Check
-
-        if call == False:
-            print("체크입니다!")
+        print("체크입니다!")
         beforeBetting = 1
         return bettingMoney
 
     elif selectMenu == 2: #Call
         print("콜입니다!")
-        if beforeBetting != 5:
-            return betting(beforeBetting, user, tableMoney, True)
+        bettingMoney = beforeCredit
+        if user.playerMoney > bettingMoney :
+            user.playerMoney -= bettingMoney
+            print(bettingMoney, "$ 베팅했습니다")
+            return bettingMoney
         else:
-             return betting(5, user, tableMoney, True)
+            return betting(-1, user, tableMoney)
 
     elif selectMenu == 3: #Half
         beforeBetting = 3
         print("레이즈! 하프입니다!")
-        if user.money > tableMoney //2 :
-            user.money -= tableMoney // 2
+        if user.playerMoney > tableMoney //2 :
+            user.playerMoney -= tableMoney // 2
             bettingMoney = tableMoney // 2
+            beforeCredit = bettingMoney
             print(bettingMoney, "$ 베팅했습니다")
             return bettingMoney
         else:
-            return betting(-1, user, tableMoney, False)
+            return betting(-1, user, tableMoney)
 
     elif selectMenu == 4: #quarter
         beforeBetting = 4
-        if call == False:
-            print("레이즈! 쿼터입니다!")
-        if user.money > tableMoney //2 :
-            user.money -= tableMoney // 4
+        print("레이즈! 쿼터입니다!")
+        if user.playerMoney > tableMoney //2 :
+            user.playerMoney -= tableMoney // 4
             bettingMoney = tableMoney // 4
+            beforeCredit = bettingMoney
             print(bettingMoney, "$ 베팅했습니다")
             return bettingMoney
         else:
-            return betting(-1, user, tableMoney, False)
+            return betting(-1, user, tableMoney)
 
     elif selectMenu == 5: #Allin
         beforeBetting = 5
-        beforeCredit = user.money
-        user.money -= beforeCredit
+        beforeCredit = user.playerMoney
+        user.playerMoney -= beforeCredit
         bettingMoney = beforeCredit
         print(beforeCredit, "$ 베팅했습니다")
         print("올인입니다!")
         return bettingMoney
     elif selectMenu == -1: #Allin on by betting
-        if beforeCredit >= user.money:
-            beforeCredit = user.money
-            user.money -= beforeCredit
+        if beforeCredit >= user.playerMoney:
+            beforeCredit = user.playerMoney
+            user.playerMoney -= beforeCredit
             bettingMoney = beforeCredit
             print(beforeCredit, "$ 베팅했습니다")
             print("올인입니다!")
         else:
-            user.money -= beforeCredit
+            user.playerMoney -= beforeCredit
             bettingMoney = beforeCredit
             print(beforeCredit, "$ 베팅했습니다")
         return bettingMoney
@@ -241,11 +243,11 @@ while (len(playerName) != 1) or nickname in playerName:
         print("게임을 시작합니다.")
 
     for player in playerList:
-        player.money = (BASEMONEY[choiceBaseMoney - 1] - baseBetting)
+        player.playerMoney = (BASEMONEY[choiceBaseMoney - 1] - baseBetting)
         tableMoney += baseBetting
         for count in range(4):
             player.playerHand.append(number_to_card(cardInstance.card_draw(),player.playerHandNum, player.playerHandPattern))
-        print_player_hand(player.playerHand,playerName[playerList.index(player)])
+    print_player_hand(playerList[0].playerHand,playerName[0])
 
     #카드 한장 버리기
     print("1:" + playerList[playerName.index(nickname)].playerHand[0], "2:" + playerList[playerName.index(nickname)].playerHand[1], "3:" + playerList[playerName.index(nickname)].playerHand[2], "4:" + playerList[playerName.index(nickname)].playerHand[3])
@@ -265,7 +267,7 @@ while (len(playerName) != 1) or nickname in playerName:
         player.handCardList_Clear()
         makeHandCard(player.handCardList, player.playerHandNum, player.playerHandPattern)
         player.playerHandRanking = cardInstance.calculate_ranking(player.handCardList, player.playerHandNum, player.playerHandPattern)
-        print(playerName[playerList.index(player)], "님의 패는", cardInstance.calculate_ranking(player.handCardList, player.playerHandNum, player.playerHandPattern), "입니다.")
+    print(playerName[0], "님의 패는", cardInstance.calculate_ranking(playerList[0].handCardList, playerList[0].playerHandNum, playerList[0].playerHandPattern), "입니다.")
 
     #카드 한장 오픈하기
     print("1:" + playerList[playerName.index(nickname)].playerHand[0], "2:" + playerList[playerName.index(nickname)].playerHand[1], "3:" + playerList[playerName.index(nickname)].playerHand[2])
@@ -295,63 +297,66 @@ while (len(playerName) != 1) or nickname in playerName:
         computer.playerHand[openCard -1] = computer.playerHand[openCard - 1] + "(open)"
 
     print_player_hand(playerList[playerName.index(nickname)].playerHand,playerName[0])
-    inputWating = input
 
     for player in playerList:
             print_open_card(player.openCardList,playerName[playerList.index(player)])
 
+    inputWating = input("계속하시려면 (enter)를 눌러주세요...") system('clear')
+
 
     #오픈 카드 3장 주기.
+    betters = playerList[:]
+    bettersName = playerName[:]
     for playTurn in range(4, 7):
-        for player in playerList:
-            if len(playerName) <= 1:
-                highestRankUser = playerName[0]
+        for player in betters:
+            if len(bettersName) <= 1:
+                highestRankUser = bettersName[0]
+
             else:
                 player.playerHand.append(number_to_card(cardInstance.card_draw(),player.playerHandNum, player.playerHandPattern))
-                print(playerName[playerList.index(player)],"님의", playTurn, "번째 카드는", player.playerHand[-1], "입니다.")
                 player.openCardList.append(player.playerHand[-1])
                 player.playerHand[-1] = player.playerHand[-1] + "(open)"
-                print_player_hand(player.playerHand, playerName[playerList.index(player)])
 
             #현재 패 족보 계산.
             player.handCardList_Clear()
             makeHandCard(player.handCardList, player.playerHandNum, player.playerHandPattern)
-            rankList.append([playerName[playerList.index(player)],cardInstance.calculate_ranking(player.handCardList, player.playerHandNum, player.playerHandPattern)])
+            rankList.append([bettersName[betters.index(player)],cardInstance.calculate_ranking(player.handCardList, player.playerHandNum, player.playerHandPattern)])
             player.playerHandRanking = cardInstance.calculate_ranking(player.handCardList, player.playerHandNum, player.playerHandPattern)
-            print(playerName[playerList.index(player)], "님의 패는",cardInstance.calculate_ranking(player.handCardList, player.playerHandNum, player.playerHandPattern), "입니다.")
+        if betters[0] == playerList[0]:
+            print(bettersName[0],"님의", playTurn, "번째 카드는", betters[0].playerHand[-1], "입니다.")
+            print_player_hand(betters[bettersName.index(nickname)].playerHand,bettersName[0])
+            print(bettersName[0], "님의 패는", cardInstance.calculate_ranking(betters[0].handCardList, betters[0].playerHandNum, betters[0].playerHandPattern), "입니다.")
+            for player in betters:
+                print_open_card(player.openCardList,bettersName[betters.index(player)])
 
-            for player in playerList:
-                print_open_card(player.openCardList,playerName[playerList.index(player)])
+        inputWating = input("계속하시려면 (enter)를 눌러주세요...") system('clear')
 
-        betters = playerList[:]
-        bettersName = playerName[:]
 
     #---------------------베팅----------------------------------------
-        print(rankList)
         if len(bettersName) <= 1:
             highestRankUser = bettersName[0]
         else:
             highestRankUser = rankList[compareRank(rankList)][0]
-            print(highestRankUser)
             firstBettingUser = betters[bettersName.index(highestRankUser)]
 
             print("tablemoney = ", tableMoney)
             if highestRankUser == nickname:
                 print("베팅을 선택해주세요.", end = '')
-                print("               현재 플레이어의 소유 머니 :", playerList[playerName.index(highestRankUser)].money)
+                print("               현재 플레이어의 소유 머니 :", playerList[bettersName.index(highestRankUser)].playerMoney)
                 playerBetting = int(input("1.check, 3.half, 4.quarter, 5.allIn, 6(or Other).fold :"))
                 if playerBetting == 2:
-                    tableMoney += betting(6, betters[bettersName.index(highestRankUser)], tableMoney, False)
+                    tableMoney += betting(6, betters[bettersName.index(highestRankUser)], tableMoney)
 
                 else:
-                    tableMoney += betting(playerBetting, betters[bettersName.index(highestRankUser)], tableMoney, False)
+                    tableMoney += betting(playerBetting, betters[bettersName.index(highestRankUser)], tableMoney)
 
             #컴퓨터가 선일때
             else:
                 print(highestRankUser, "의 베팅 턴입니다.")
                 playerBetting = computer_betting(firstBettingUser,True)#컴퓨터 베팅 알고리즘.
-                tableMoney += betting(playerBetting, betters[bettersName.index(highestRankUser)], tableMoney, False)
+                tableMoney += betting(playerBetting, betters[bettersName.index(highestRankUser)], tableMoney)
 
+            inputWating = input("계속하시려면 (enter)를 눌러주세요...") system('clear')
 
 
             #후 베팅
@@ -363,53 +368,54 @@ while (len(playerName) != 1) or nickname in playerName:
                     print("tablemoney = ", tableMoney)
                     if BettingUser == nickname:
                         print("베팅을 선택해주세요.", end = '')
-                        print("               현재 플레이어의 소유 머니 :", betters[bettersName.index(BettingUser)].money)
+                        print("               현재 플레이어의 소유 머니 :", betters[bettersName.index(BettingUser)].playerMoney)
                         playerBetting = int(input("2.call, 3.half, 4.quarter, 5.allIn, 6(or Other).fold :"))
                         if playerBetting == 1:
-                            tableMoney += betting(6, betters[bettersName.index(BettingUser)], tableMoney, False)
+                            tableMoney += betting(6, betters[bettersName.index(BettingUser)], table)
 
                         else:
-                            tableMoney += betting(playerBetting, betters[bettersName.index(BettingUser)], tableMoney, False)
+                            tableMoney += betting(playerBetting, betters[bettersName.index(BettingUser)], tableMoney)
 
                     else:
                         print(BettingUser, "의 베팅 턴입니다.")
                         playerBetting = computer_betting(betters[bettersName.index(BettingUser)], False)#컴퓨터 베팅 알고리즘.
-                        tableMoney += betting(playerBetting, betters[bettersName.index(BettingUser)], tableMoney, False)
+                        tableMoney += betting(playerBetting, betters[bettersName.index(BettingUser)], tableMoney)
 
+        inputWating = input("계속하시려면 (enter)를 눌러주세요...") system('clear')
 
         rankList = []
+        beforeCredit = 0
         #-----------------------------------------------------------
 
 
         #히든 카드 1장 주기.
     print("7번째 히든 카드 입니다.")
-    for player in playerList:
+    for player in betters:
         player.playerHand.append(number_to_card(cardInstance.card_draw(),player.playerHandNum, player.playerHandPattern))
-        print_player_hand(player.playerHand, playerName[playerList.index(player)])
+        print_player_hand(player.playerHand, bettersName[betters.index(player)])
 
-        rankList.append([playerName[playerList.index(player)],cardInstance.calculate_ranking(player.handCardList, player.playerHandNum, player.playerHandPattern)])
+        rankList.append([bettersName[betters.index(player)],cardInstance.calculate_ranking(player.handCardList, player.playerHandNum, player.playerHandPattern)])
         player.playerHandRanking = cardInstance.calculate_ranking(player.handCardList, player.playerHandNum, player.playerHandPattern)
-        print(playerName[playerList.index(player)], "님의 패는",cardInstance.calculate_ranking(player.handCardList, player.playerHandNum, player.playerHandPattern), "입니다.")
-
-        for player in playerList:
-            print_open_card(player.openCardList,playerName[playerList.index(player)])
-
-
         player.handCardList_Clear()
         makeHandCard(player.handCardList, player.playerHandNum, player.playerHandPattern)
-        print(playerName[playerList.index(player)], "님의 패는", cardInstance.calculate_ranking(player.handCardList, player.playerHandNum, player.playerHandPattern), "입니다.")
-    if len(playerName) <= 1:
-        highestRankUser = playerName[0]
+        print(bettersName[betters.index(player)], "님의 패는", cardInstance.calculate_ranking(player.handCardList, player.playerHandNum, player.playerHandPattern), "입니다.")
+
+    if len(bettersName) <= 1:
+        highestRankUser = bettersName[0]
     else:
-        print(compareRank(rankList))
-        print(rankList)
-        print(rankList[compareRank(rankList)][0])
         highestRankUser = rankList[compareRank(rankList)][0]
     print("승자는", highestRankUser, "입니다.")
     #이후 우승자에게 테이블 머니만큼 돈 추가.
     #userList 원상복구(Gamelist 이용)
     cardInstance.generate_deck()
-    playerList[playerName.index(highestRankUser)].playerMoney += tableMoney
+    betters[bettersName.index(highestRankUser)].playerMoney += tableMoney
     for i in playerList:
         print(playerName[playerList.index(i)],"님의 현재 소지 금액은", i.playerMoney, "$ 입니다.")
+        player.clear()
+    QuestionGame = input("종료하시려면 1번을, 계속하시려면 그 외 아무키나 눌러주세요. ")
+    if QuestionGame == "1":
+        exit()
+    else:
+        pass
+
     #반복.
